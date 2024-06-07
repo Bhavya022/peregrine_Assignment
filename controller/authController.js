8831db7172cc9361265418661b5adcfcd030994e
Bhavya022 <dnd88778@gmail.com>
2024-06-07 14:46:13 +0530

    donee

--- controller/authController.js
+++ controller/authController.js
@@ -4,8 +4,9 @@ const multer  = require('multer')
   exports.register = async(req,res)=>{
     try{
       console.log(req.body);
+
       //const {Image}=req.file.path;
-      //console.log(name,email,age,Image)  
+      //console.log(name,email,age)  
       const User1 = new User({ 
        name: req.body.name,
        email: req.body.Email,
--- route/authRoutes.js
+++ route/authRoutes.js
@@ -2,7 +2,7 @@
 const authController = require('../controller/authController');
 const AgeMiddleware = require('../middleware/AgeMiddleware')
 const express = require('express');
-const app = express(); 
+const app = express.Router(); 
 console.log(authController) 
 app.post('/register',authController.register); 
 
